c9d88aa85db5a70cf563254d779d2db9dcb9f29e
Claudio Antonio Peanho <clpeanho@gmail.com>
2017-11-14 22:40:26 -0200

    Add routing by path

--- frontend/src/containers/Root.js
+++ frontend/src/containers/Root.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 // import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 
 class Root extends Component {
   render() {
     return (
-      <App />
+      <BrowserRouter>
+        <Route exact path="/" component={App} />
+      </BrowserRouter>
     );
   }
 }
