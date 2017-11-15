65f058dbea6ce7b13bcda805af6bafee63012dc8
Claudio Antonio Peanho <clpeanho@gmail.com>
2017-11-14 22:25:31 -0200

    Apply presentational and container component structure

---
+++ frontend/src/containers/Root.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import { Provider } from 'react-redux';
+import App from './App';
+
+class Root extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+export default Root;
